9d495c611705c383bb6b257e2e22a0e79f3dc316
zouqilin <mr.zouqilin@gmail.com>
2016-12-13 18:32:36 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidekiqCronConcurrencyTest::Application.config.secret_token = 'bad551b300e1397d051f3c462b6ec085794b09afdc88694de0285d41534e80de492a5552525657739097607ba0327da9e612cbb52ddcb0d042d3da100dff26d6'
